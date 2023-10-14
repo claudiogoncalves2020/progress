3c669f741f762a9d191065832f742772fba39381
Claudio Goncalves <claudiojmg1@hotmail.de>
2023-10-14 18:16:23 +0200

    #

---
+++ script.js
@@ -0,0 +1,11 @@
+let number = document.getElementById("number");
+let counter = 0;
+
+setInterval(() => {
+  if (counter == 80) {
+    clearInterval();
+  } else {
+    counter += 1;
+    number.innerHTML = counter + "%";
+  }
+}, 23);
